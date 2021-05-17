b5e27ee1779245a25344a8d2634816d8061aae69
nourdine <nourdine71183@gamil.com>
2021-05-17 15:21:49 +0200

    responsive desktop

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -20,6 +20,7 @@ async function GetData() {
 };
 
 
+
 document.addEventListener("DOMContentLoaded", async function (event) {
     datas = await GetData()
     const photographers=hashtagfilters(datas.photographers)
@@ -36,28 +37,30 @@ function Addphotographers(photographers) {
     let photographeContainer = document.getElementsByClassName("photographe-container")
     photographers.forEach(element => {
         const photographecard = `
-          <a href="photographe.html?id=${element.id}" class="photographe-card">
-          <div class="image">
-              <!--insertion de l'imge en js-->
-              <img class="pics" src="./assets/image/SamplePhotos/PhotographersIDPhotos/${element.portrait}" alt="">
-          </div>
-          <div class="photographe-description">
-              <h2 class="photographe-name" aria-label="Nom du photographe">
-                    ${element.name}
-              </h2>
-              <div class="photographe-locate" aria-label="Localisation Géographique">
-                  ${element.city}, ${element.country}
-              </div>
-              <div class="photographe-descr">
-                ${element.tagline}
-              </div>
-              <div class="photographe-price" aria-label="Prix de la prestation">
-              ${element.price}$
-              </div>
-          </div>
-      </a>
-        <ul class="hashtag-${element.id} hashtag-ul">   
-        </ul>
+        <div class="photographe-card">
+            <a href="photographe.html?id=${element.id}" class="photographe-card">
+                <div class="image">
+                    <!--insertion de l'imge en js-->
+                    <img class="pics" src="./assets/image/SamplePhotos/PhotographersIDPhotos/${element.portrait}" alt="">
+                </div>
+                <div class="photographe-description">
+                    <h2 class="photographe-name" aria-label="Nom du photographe">
+                            ${element.name}
+                    </h2>
+                    <div class="photographe-locate" aria-label="Localisation Géographique">
+                        ${element.city}, ${element.country}
+                    </div>
+                    <div class="photographe-descr">
+                        ${element.tagline}
+                    </div>
+                    <div class="photographe-price" aria-label="Prix de la prestation">
+                    ${element.price}$
+                    </div>
+                </div>
+            </a>
+            <ul class="hashtag-${element.id} hashtag-ul">   
+            </ul>
+        </div>
       `
         photographeContainer[0].insertAdjacentHTML("beforeend", photographecard)
         AddTag(element.tags, element.id)
@@ -71,7 +74,7 @@ function AddTag(tags, photographerID) {
     let tagcontainer = document.getElementsByClassName(`hashtag-${photographerID}`)
     tags.forEach(element => {
         const tag = `
-        <li class="photographe-tag"><a href="${element}">#${element}</a></li>
+        <li class="photographe-tag"><a href="#${element}">#${element}</a></li>
         `
         tagcontainer[0].insertAdjacentHTML("beforeend", tag)
     })
