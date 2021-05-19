4a2bd1b2f95f7634007e5691eb683433519c30b0
nourdine <nourdine71183@gamil.com>
2021-05-19 12:43:08 +0200

    fetch

--- assets/Js/photographe.js
+++ assets/Js/photographe.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async function (event) {
 });
 
 async function GetData() {
-    const responsemock = await fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json")
+    const responsemock = await fetch("./assets/Js/mock.json")
         if (responsemock.ok) {
             const responsejson= await responsemock.json()
             return responsejson
