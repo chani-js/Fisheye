02d90c67e439781a65ae5034aa1901046a2c46c3
nourdine <nourdine71183@gamil.com>
2021-05-19 12:23:05 +0200

    test fetch

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -11,7 +11,7 @@ let filters= {
     events: true
 }
 async function GetData() {
-    const responsemock = await fetch("./assets/Js/mock.json")
+    const responsemock = await fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json")
         if (responsemock.ok) {
             const responsejson= await responsemock.json()
             return responsejson
